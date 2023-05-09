bc4a327b93c2e4a4b2ee2bd23eb79b44a7f3a7c8
Thanatix <thomaslambert@kinetix.tech>
2023-05-09 11:00:02 +0200

    v0.6.1

--- Runtime/Const/KinetixConstants.cs
+++ Runtime/Const/KinetixConstants.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // // ----------------------------------------------------------------------------
 // // <copyright file="BlockchainConstants.cs" company="Kinetix SAS">
 // // Kinetix Unity SDK - Copyright (C) 2022 Kinetix SAS
@@ -27,3 +28,49 @@ namespace Kinetix.Internal
 
     }
 }
+=======
+// // ----------------------------------------------------------------------------
+// // <copyright file="BlockchainConstants.cs" company="Kinetix SAS">
+// // Kinetix Unity SDK - Copyright (C) 2022 Kinetix SAS
+// // </copyright>
+// // ----------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Kinetix.Internal
+{
+    public static class KinetixConstants
+    {
+        public const string version = "0.6.1";
+
+        public static bool C_ShouldUGCBeAvailable = true;
+        
+#if STAGING_KINETIX
+        public static readonly string c_SDK_API_URL = "https://sdk-api.staging.kinetix.tech";
+#else
+        public static readonly string c_SDK_API_URL = "https://sdk-api.kinetix.tech";
+#endif
+
+
+        public static readonly int c_TimeOutCreateQRCode = 305;
+        
+        
+        #region StreamingAssets
+        
+#if DEV_KINETIX
+        public const string C_FreeAnimationsAssetPluginPath = "Assets/Plugins/Kinetix/Core/Resources/FreeAnimations"; 
+#elif WEB2
+        public const string C_FreeAnimationsAssetPluginPath = "Packages/com.kinetix.coreweb2/Resources/FreeAnimations";
+#endif
+        
+        public static string C_FreeAnimationsManifestPath => "Kinetix/FreeEmoteManifest.json";
+        public const string C_FreeAnimationsAssetSAPath = "Assets/StreamingAssets/Kinetix/FreeAnimations";
+        public const string C_FreeAnimationsPath = "Kinetix/FreeAnimations";
+        public const string C_FreeCustomAnimationsPath = "Kinetix/CustomFreeAnimations";
+        public const string C_FreeCustomAnimationsAssetSAPath = "Assets/StreamingAssets/Kinetix/CustomFreeAnimations";
+        
+        #endregion
+
+    }
+}
+>>>>>>> Stashed changes
