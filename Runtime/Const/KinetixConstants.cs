ec5c5c2b762d092aab1bd8db4d63cb21042c59b6
Thanatix <thomaslambert@kinetix.tech>
2023-05-09 11:02:31 +0200

    v0.6.1

--- Runtime/Const/KinetixConstants.cs
+++ Runtime/Const/KinetixConstants.cs
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-// // ----------------------------------------------------------------------------
-// // <copyright file="BlockchainConstants.cs" company="Kinetix SAS">
-// // Kinetix Unity SDK - Copyright (C) 2022 Kinetix SAS
-// // </copyright>
-// // ----------------------------------------------------------------------------
-
-
-using System.Collections.Generic;
-
-namespace Kinetix.Internal
-{
-    public static class KinetixConstants
-    {
-        public const string version = "0.5.1";
-        
-        #region StreamingAssets
-        
-        public const string C_FreeAnimationsAssetPluginPath = "Packages/com.kinetix.coreweb2/Resources/FreeAnimations";
-        
-        public static string C_FreeAnimationsManifestPath => "Kinetix/FreeEmoteManifest.json";
-        public const string C_FreeAnimationsAssetSAPath = "Assets/StreamingAssets/Kinetix/FreeAnimations";
-        public const string C_FreeAnimationsPath = "Kinetix/FreeAnimations";
-        public const string C_FreeCustomAnimationsPath = "Kinetix/CustomFreeAnimations";
-        public const string C_FreeCustomAnimationsAssetSAPath = "Assets/StreamingAssets/Kinetix/CustomFreeAnimations";
-        
-        #endregion
-
-    }
-}
-=======
-=======
->>>>>>> 06ddde23de98092f4c3f29a5edcf5fb88da8c702
 // // ----------------------------------------------------------------------------
 // // <copyright file="BlockchainConstants.cs" company="Kinetix SAS">
 // // Kinetix Unity SDK - Copyright (C) 2022 Kinetix SAS
@@ -44,11 +10,7 @@ namespace Kinetix.Internal
 {
     public static class KinetixConstants
     {
-<<<<<<< HEAD
         public const string version = "0.6.1";
-=======
-        public const string version = "0.6.0";
->>>>>>> 06ddde23de98092f4c3f29a5edcf5fb88da8c702
 
         public static bool C_ShouldUGCBeAvailable = true;
         
@@ -64,15 +26,11 @@ namespace Kinetix.Internal
         
         #region StreamingAssets
         
-<<<<<<< HEAD
 #if DEV_KINETIX
         public const string C_FreeAnimationsAssetPluginPath = "Assets/Plugins/Kinetix/Core/Resources/FreeAnimations"; 
 #elif WEB2
         public const string C_FreeAnimationsAssetPluginPath = "Packages/com.kinetix.coreweb2/Resources/FreeAnimations";
 #endif
-=======
-        public const string C_FreeAnimationsAssetPluginPath = "Packages/com.kinetix.coreweb2/Resources/FreeAnimations";
->>>>>>> 06ddde23de98092f4c3f29a5edcf5fb88da8c702
         
         public static string C_FreeAnimationsManifestPath => "Kinetix/FreeEmoteManifest.json";
         public const string C_FreeAnimationsAssetSAPath = "Assets/StreamingAssets/Kinetix/FreeAnimations";
@@ -84,7 +42,3 @@ namespace Kinetix.Internal
 
     }
 }
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> 06ddde23de98092f4c3f29a5edcf5fb88da8c702
